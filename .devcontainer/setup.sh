8b7191fe7772bf06592f6bcc2f765ad2abd3e608
daman-d4rd0 <echaverocano@gmail.com>
2026-08-28 09:30:18 -0600

    paso bug 4

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -10,11 +10,14 @@ echo "📦 Iniciando la configuración de Apache Zeppelin..."
 # Definir rutas
 ZEPPELIN_TARGET_DIR="/home/vscode/zeppelin"
 TARGET_DIR="/workspaces"
+ARCHIVO="zeppelin-0.12.1-bin-all.tgz"
 # Comprobar si Zeppelin ya está instalado para no repetir la descarga si el contenedor se reconstruye
 if [ ! -d "$ZEPPELIN_TARGET_DIR" ]; then
     echo "⬇️ Descargando Zeppelin (usando tu script original)..."
     URL="https://dlcdn.apache.org/zeppelin/zeppelin-0.12.1/zeppelin-0.12.1-bin-all.tgz"
     wget "$URL" "$TARGET_DIR"
+    mkdir "$ZEPPELIN_TARGET_DIR"
+    tar -xvf $ARCHIVO -C "$ZEPPELING_TARGET_DIR"
     # Ejecutamos tu script original de descarga directamente desde su ubicación actual
     # Esto evita tener que copiarlo manualmente con 'cp'
     # bash ./config/download_zeppelin.sh 
