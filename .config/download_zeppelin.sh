cef389aa5d838ecf45a5fe665ed5acaf131a4938
daman-d4rd0 <echaverocano@gmail.com>
2026-08-28 12:44:06 -0600

    paso bug  30

--- .config/download_zeppelin.sh
+++ .config/download_zeppelin.sh
@@ -9,12 +9,10 @@ ZEPPELIN_TARGET_DIR="/home/vscode/zeppelin"
 DOWNLOAD_ZEPPELIN="/workspaces/Portafolio_Laboral"
 NOMBRE_ARCHIVO="zeppelin-0.12.1-bin-all"
 ARCHIVO="zeppelin-0.12.1-bin-all.tgz"
- if [ -d "$ZEPPELIN_TARGET_DIR" ]; then
-    tar -xvf "$DOWNLOAD_ZEPPELIN/$ARCHIVO" -C "$ZEPPELIN_TARGET_DIR"
-    rm -rf "$ARCHIVO"
-    bash "$ZEPPELIN_TARGET_DIR/$NOMBRE_ARCHIVO/bin/zeppelin-daemon.sh start"
-else
-    echo "ℹ️ Zeppelin ya se encuentra instalado en $ZEPPELIN_TARGET_DIR Omitiendo descarga."
-    bash "$ZEPPELIN_TARGET_DIR/$NOMBRE_ARCHIVO/bin/zeppelin-daemon.sh start"
-    rm -rf "$ARCHIVO"
-fi
+tar -xvf "$DOWNLOAD_ZEPPELIN/$ARCHIVO"
+mv "$DOWNLOAD_ZEPPELIN/$ARCHIVO" "$ZEPPELIN_TARGET_DIR"
+rm -rf "$ARCHIVO"
+
+piython -m venv "$ZEPPELIN_TARGET_DIR/virtual"  pip install --no-cache-dir -r "$DOWNLOAD_ZEPPELIN/requirements.txt"
+
+source "$ZEPPELIN_TARGET_DIR/bin/activate"
--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -2,23 +2,9 @@
 # Detener la ejecución si ocurre cualquier error durante el proceso
 set -e
 set -x
-echo "🐍 Instalando dependencias de Python..."
-pip3 install --no-cache-dir -r requirements.txt
 
-echo "📦 Iniciando la configuración de Apache Zeppelin..."
+#pip3 install --no-cache-dir -r requirements.txt
 
-# Definir rutas
-ZEPPELIN_TARGET_DIR="/home/vscode/zeppelin"
-DOWNLOAD_ZEPPELIN="/workspaces/Portafolio_Laboral"
-ARCHIVO="zeppelin-0.12.1-bin-all.tgz"
-# Comprobar si Zeppelin ya está instalado para no repetir la descarga si el contenedor se reconstruye
-if [ ! -d "$ZEPPELIN_TARGET_DIR" ]; then
-    echo "⬇️ Descargando Zeppelin (usando tu script original)..."
-    mkdir -p "$ZEPPELIN_TARGET_DIR"
-    URL="https://dlcdn.apache.org/zeppelin/zeppelin-0.12.1/zeppelin-0.12.1-bin-all.tgz"
-    wget "$URL"
-    
-    echo "✅ Descarga completa"
-else
-    echo "ℹ️ Zeppelin ya se encuentra instalado en $ZEPPELIN_TARGET_DIR Omitiendo descarga."
-fi
+echo "Iniciando la configuración de Apache Zeppelin..."
+URL="https://dlcdn.apache.org/zeppelin/zeppelin-0.12.1/zeppelin-0.12.1-bin-all.tgz"
+wget "$URL"
