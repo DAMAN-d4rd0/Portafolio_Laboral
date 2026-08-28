03b7a3d723509ae15053c86399aee2017f1522fe
daman-d4rd0 <echaverocano@gmail.com>
2026-08-28 10:55:24 -0600

    paso bug 17

--- .config/download_zeppelin.sh
+++ .config/download_zeppelin.sh
@@ -9,8 +9,10 @@ DOWNLOAD_ZEPPELIN="/workspaces/Portafolio_Laboral"
 ARCHIVO="zeppelin-0.12.1-bin-all.tgz"
  if [ ! -d "$ZEPPELIN_TARGET_DIR" ]; then
     tar -xvf "$DOWNLOAD_ZEPPELIN/$ARCHIVO" -C "$ZEPPELIN_TARGET_DIR"
+    rm -rf "$ARCHIVO"
    # /home/vscode/zeppelin/bin/zeppelin-daemon.sh start
 # echo "¡Proceso terminado con éxito!"
 else
     echo "ℹ️ Zeppelin ya se encuentra instalado en $ZEPPELIN_TARGET_DIR Omitiendo descarga."
+    rm -rf "$ARCHIVO"
 fi
