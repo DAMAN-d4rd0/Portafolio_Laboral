a1d12a17330710157fd14edc500704d3a1f9bf62
daman-d4rd0 <echaverocano@gmail.com>
2026-08-28 08:28:58 -0600

    paso de zeppelin

--- .config/download_zeppelin.sh
+++ .config/download_zeppelin.sh
@@ -8,23 +8,23 @@ URL="https://dlcdn.apache.org/zeppelin/zeppelin-0.12.1/zeppelin-0.12.1-bin-all.t
 ARCHIVO="zeppelin-0.12.1-bin-all.tgz"
 CARPETA_ORIGINAL="zeppelin-0.12.1-bin-all" # <- El nombre que ya viene dentro del tar
 CARPETA_NUEVA="zeppeling"                  # <- El nombre que tú quieres
-RUTA_ZEPPELING="/home/vscode"
+RUTA_ZEPPELING="/home/vscode/"
 echo "1. Descargando archivo..."
 cd "$RUTA_ZEPPELING"
 wget -q "$URL"
 
 echo "2. Descomprimiendo archivo..."
-tar -zxvf "$ARCHIVO"
+tar -zvf "$ARCHIVO"
 
-echo "3. Renombrando carpeta de '$CARPETA_ORIGINAL' a '$CARPETA_NUEVA'..."
-if [ -d "$CARPETA_ORIGINAL" ]; then
-    mv "$CARPETA_ORIGINAL" "$CARPETA_NUEVA"
-else
-    echo "Error: La carpeta '$CARPETA_ORIGINAL' no existe. Verifica su nombre."
-    exit 1
-fi
+# echo "3. Renombrando carpeta de '$CARPETA_ORIGINAL' a '$CARPETA_NUEVA'..."
+# if [ -d "$CARPETA_ORIGINAL" ]; then
+#     mv "$CARPETA_ORIGINAL" "$CARPETA_NUEVA"
+# else
+#     echo "Error: La carpeta '$CARPETA_ORIGINAL' no existe. Verifica su nombre."
+#     exit 1
+# fi
 
-echo "4. Borrando archivo comprimido..."
-rm "$ARCHIVO"
+# echo "4. Borrando archivo comprimido..."
+# rm "$ARCHIVO"
 
-echo "¡Proceso terminado con éxito!"
+# echo "¡Proceso terminado con éxito!"
