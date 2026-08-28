bb8f81c0ca87cad36a36f0140f96d5178301bf08
daman-d4rd0 <echaverocano@gmail.com>
2026-08-28 09:04:14 -0600

    paso bug 1

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -3,21 +3,21 @@
 set -e
 set -x
 echo "🐍 Instalando dependencias de Python..."
-pip3 install --no-cache-dir -r requirements.txt
+#pip3 install --no-cache-dir -r requirements.txt
 
 echo "📦 Iniciando la configuración de Apache Zeppelin..."
 
 # Definir rutas
 ZEPPELIN_TARGET_DIR="/home/vscode/zeppelin"
-pwd
+
 # Comprobar si Zeppelin ya está instalado para no repetir la descarga si el contenedor se reconstruye
 if [ ! -d "$ZEPPELIN_TARGET_DIR" ]; then
     echo "⬇️ Descargando Zeppelin (usando tu script original)..."
     
-    cd "ZEPPELIN_TARGET_DIR"
+
     # Ejecutamos tu script original de descarga directamente desde su ubicación actual
     # Esto evita tener que copiarlo manualmente con 'cp'
-    bash ./config/download_zeppelin.sh || true
+    bash ./config/download_zeppelin.sh 
     
     echo "✅ Zeppelin configurado correctamente."
 else
